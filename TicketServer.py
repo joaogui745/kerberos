8778c93407e39814843c27c53b317b630e1e3bc2
Guilherme <132709797+guigas1@users.noreply.github.com>
2026-07-04 13:17:20 -0300

    removendo comentarios para melhorar visulização
    Removed commented lines to improve code clarity.

--- TicketServer.py
+++ TicketServer.py
@@ -19,7 +19,7 @@ class TicketServer(threading.Thread):
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind(f"tcp://*:{self.port}")
 
-        # Tempo máximo aceito para o Authenticator_c, em segundos.
+        
         self.max_clock_skew = 300
 
     def run(self):
@@ -57,7 +57,7 @@ class TicketServer(threading.Thread):
         if service_id not in SERVICE_KEYS:
             raise Exception("Servico desconhecido")
 
-        # 1. TGS descriptografa o Ticket_tgs usando a chave secreta do TGS.
+        
         tgt = decrypt_json(TGS_SECRET_KEY, encrypted_tgt)
 
         client_id_from_tgt = tgt["client_id"]
@@ -67,11 +67,9 @@ class TicketServer(threading.Thread):
 
         now = int(time.time())
 
-        # 2. Verifica validade do Ticket_tgs.
         if now > tgt_timestamp + tgt_lifetime:
             raise Exception("Ticket_tgs expirado")
 
-        # 3. TGS descriptografa o Authenticator_c usando K_c_tgs.
         authenticator = decrypt_json(
             session_key_client_tgs,
             encrypted_authenticator
@@ -80,18 +78,18 @@ class TicketServer(threading.Thread):
         client_id_from_authenticator = authenticator["client_id"]
         authenticator_timestamp = int(authenticator["timestamp"])
 
-        # 4. Verifica se o cliente do ticket é o mesmo do autenticador.
+        #
         if client_id_from_tgt != client_id_from_authenticator:
             raise Exception("Cliente do TGT diferente do cliente do autenticador")
 
-        # 5. Verifica se o autenticador é recente.
+        
         if abs(now - authenticator_timestamp) > self.max_clock_skew:
             raise Exception("Authenticator_c expirado ou invalido")
 
-        # 6. Gera a chave de sessão Cliente-Serviço: K_c_v.
+        
         session_key_client_service = generate_key()
 
-        # 7. Cria o Ticket_v, criptografado com a chave secreta do serviço.
+        
         ticket_service_payload = {
             "client_id": client_id_from_tgt,
             "service_id": service_id,
@@ -107,8 +105,7 @@ class TicketServer(threading.Thread):
             ticket_service_payload
         )
 
-        # 8. Cria a resposta para o cliente:
-        # E(K_c_tgs, [K_c_v || ID_v || TS4 || Ticket_v])
+        
         response_payload = {
             "session_key_service": session_key_client_service,
             "service_id": service_id,
